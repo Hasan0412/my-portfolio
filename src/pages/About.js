e69f9d89f3377f52aad0bc5edf8610918eecbb11
Hasan Malik <hmalik0412@gmail.com>
2023-09-05 08:50:49 +0100

    About me updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,19 @@ import "../CSS/About.css";
 function About() {
   return (
     <section id="about">
-      <p>My name is Hasan Malik</p>
+      <p>My name is Hasan Malik. I am a high motivated individual partaking in a 24-week software bootcamp at the University of Birmingham. In hopes of switching careers, shifting my focus from retail to software development. 
+        
+My journey into software development began with a fascination for technology and a desire to create innovative solutions to real-world problems. The 24-week software bootcamp at the University of Birmingham has been an incredible learning experience, where I've had the opportunity to dive deep into various programming languages and technologies.
+
+Throughout this bootcamp, I've honed my coding skills, tackled complex challenges, and collaborated with a diverse group of talented individuals. This immersive learning environment has not only expanded my technical knowledge but has also sharpened my problem-solving abilities.
+
+My transition from a background in retail to software development has been driven by a passion for crafting elegant and efficient software solutions. I'm excited about the ever-evolving tech landscape and the endless possibilities it offers. With each line of code I write, I'm one step closer to achieving my goal of making a meaningful impact in the world of software development.
+
+In addition to coding, I'm a firm believer in continuous learning and growth. I stay up-to-date with industry trends, attend tech meetups, and eagerly seek opportunities to contribute to open-source projects. I'm enthusiastic about collaborating with like-minded professionals, sharing ideas, and exploring new horizons in the world of technology.
+
+As I embark on this exciting journey in software development, I look forward to facing new challenges, solving complex problems, and building innovative applications that make a positive difference in people's lives.
+
+Thank you for taking the time to get to know me better, and I'm excited to connect and collaborate with fellow developers and potential employers. Let's shape the future of technology together!</p>
     </section>
   );
 }
